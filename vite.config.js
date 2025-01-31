29a6a8e3100ef962d8f01945fa404f2bac7e6b09
GUILHERME FEITOSA CUNHA <126368583+guilherme273@users.noreply.github.com>
2025-01-31 14:21:21 -0300

    alguns ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Altere para o subdiretório, se necessário, ex: '/meu-projeto/'
   plugins: [react()],
-})
+});
